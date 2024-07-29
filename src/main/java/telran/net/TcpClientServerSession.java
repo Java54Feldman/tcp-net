fcc245dcbdcf5a3ade841669e15055c1b7e712ab
Pavel Feldman <feldmanpk@gmail.com>
2024-07-29 22:15:52 +0300

    CW #41 //TODO's for shutdown and closing connections for clients in idle

--- src/main/java/telran/net/TcpClientServerSession.java
+++ src/main/java/telran/net/TcpClientServerSession.java
@@ -9,6 +9,11 @@ public class TcpClientServerSession extends Thread {
 
 	public TcpClientServerSession (Socket socket, Protocol protocol) {
 		this.socket = socket;
+		//TODO
+		//using the method setSoTimeout and some solution for getting session
+		//to know about shutdown
+		//you should stop he thread after shutdown command
+		
 		this.protocol = protocol;
 	}
 
@@ -16,11 +21,19 @@ public class TcpClientServerSession extends Thread {
 		try (BufferedReader receiver = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				PrintStream sender = new PrintStream(socket.getOutputStream())) {
 			String line = null;
+			//FIXME
+			//figure out solution 
+			//for exiting from the thread after shutdown
 			while ((line = receiver.readLine()) != null) {
 				String responseStr = protocol.getResponseWithJSON(line);
 				sender.println(responseStr);
 			}
-
+			//TODO 
+			//handling SocketTimeoutException for exiting
+			//from the thread on two conditions:
+			//1. Shutdown has been performed
+			//2. Thread exists in IDLE state more than 1 min
+			//exiting from the cycle should be followed by closing connection	
 		} catch (Exception e) {
 			System.out.println(e);
 		}
--- src/main/java/telran/net/TcpServer.java
+++ src/main/java/telran/net/TcpServer.java
@@ -5,21 +5,31 @@ import java.net.*;
 public class TcpServer {
 	Protocol protocol;
 	int port;
+	boolean running = true;
 	public TcpServer(Protocol protocol, int port) {
 		this.protocol = protocol;
 		this.port = port;
 	}
 	public void run() {
 		try(ServerSocket serverSocket = new ServerSocket(port)) {
+			//TODO 
+			//using ServerSocket has the method 
+			//setSoTimeout
 			System.out.println("Server is listening on port " + port);
-			while(true) {
+			while(running) {
 				Socket socket = serverSocket.accept();
 				TcpClientServerSession session = new TcpClientServerSession(socket, protocol);
 				session.start();
+
 			}
+			//TODO
+			//handling timeout exception
 		} catch(Exception e) {
 			throw new RuntimeException(e);
 		}
+	}
+	public void shutdown() {
+		running = false;
 	}
 	
 }
