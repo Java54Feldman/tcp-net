41685e6b8e37d7d721f9b9fa49eac2230bc3dbd6
Pavel Feldman <feldmanpk@gmail.com>
2024-07-31 17:14:07 +0300

    HW #41 added multi-threading & shutdown TcpServer

--- src/main/java/telran/net/TcpClientServerSession.java
+++ src/main/java/telran/net/TcpClientServerSession.java
@@ -3,40 +3,56 @@ package telran.net;
 import java.net.*;
 import java.io.*;
 
-public class TcpClientServerSession extends Thread {
+public class TcpClientServerSession implements Runnable {
 	Socket socket;
 	Protocol protocol;
+	private static final int IDLE_TIMEOUT = 60000;
+	private boolean isShutdown = false;
+	// using the method setSoTimeout and some solution for getting session
+	// to know about shutdown
+	// you should stop the thread after shutdown command
 
-	public TcpClientServerSession (Socket socket, Protocol protocol) {
+	public TcpClientServerSession(Socket socket, Protocol protocol) {
 		this.socket = socket;
-		//TODO
-		//using the method setSoTimeout and some solution for getting session
-		//to know about shutdown
-		//you should stop he thread after shutdown command
-		
 		this.protocol = protocol;
+		try {
+			this.socket.setSoTimeout(IDLE_TIMEOUT);
+		} catch (SocketException e) {
+
+		}
 	}
 
+	@Override
 	public void run() {
 		try (BufferedReader receiver = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				PrintStream sender = new PrintStream(socket.getOutputStream())) {
 			String line = null;
-			//FIXME
-			//figure out solution 
-			//for exiting from the thread after shutdown
-			while ((line = receiver.readLine()) != null) {
+			// exiting from the thread after shutdown
+			// handling SocketTimeoutException for exiting from the thread on two conditions:
+			// 1. Shutdown has been performed
+			// 2. Thread exists in IDLE state more than 1 min
+			// exiting from the cycle should be followed by closing connection
+			while (!isShutdown && (line = receiver.readLine()) != null) {
 				String responseStr = protocol.getResponseWithJSON(line);
 				sender.println(responseStr);
 			}
-			//TODO 
-			//handling SocketTimeoutException for exiting
-			//from the thread on two conditions:
-			//1. Shutdown has been performed
-			//2. Thread exists in IDLE state more than 1 min
-			//exiting from the cycle should be followed by closing connection	
+		} catch (SocketTimeoutException e) {
+			System.out.println("Session timed out due to inactivity");
+
 		} catch (Exception e) {
 			System.out.println(e);
+		} finally {
+			try {
+				socket.close();
+			} catch (IOException e) {
+				System.out.println("Error closing socket: " + e.getMessage());
+			}
 		}
+
 	}
+	
+	public void shutdown() {
+        isShutdown = true; 
+    }
 
 }
--- src/main/java/telran/net/TcpServer.java
+++ src/main/java/telran/net/TcpServer.java
@@ -1,35 +1,47 @@
 package telran.net;
 
 import java.net.*;
+import java.util.*;
 
-public class TcpServer {
+public class TcpServer implements Runnable {
 	Protocol protocol;
 	int port;
 	boolean running = true;
+	private static final int ACCEPT_TIMEOUT = 1000; 
+    private List<TcpClientServerSession> activeSessions = new ArrayList<>();
+    
 	public TcpServer(Protocol protocol, int port) {
 		this.protocol = protocol;
 		this.port = port;
 	}
 	public void run() {
 		try(ServerSocket serverSocket = new ServerSocket(port)) {
-			//TODO 
-			//using ServerSocket has the method 
-			//setSoTimeout
+			//using ServerSocket has the method setSoTimeout
+			serverSocket.setSoTimeout(ACCEPT_TIMEOUT);
 			System.out.println("Server is listening on port " + port);
 			while(running) {
-				Socket socket = serverSocket.accept();
-				TcpClientServerSession session = new TcpClientServerSession(socket, protocol);
-				session.start();
+				try {
+					Socket socket = serverSocket.accept();
+					TcpClientServerSession session = new TcpClientServerSession(socket, protocol);
+					activeSessions.add(session);
+					session.run();
+				} catch (SocketTimeoutException e) {
 
+				}
 			}
-			//TODO
-			//handling timeout exception
+			
 		} catch(Exception e) {
 			throw new RuntimeException(e);
-		}
+		} finally {
+			shutdown();
+        }
 	}
 	public void shutdown() {
 		running = false;
+        for (TcpClientServerSession session : activeSessions) {
+            session.shutdown();
+        }
+        activeSessions.clear();
 	}
 	
 }
