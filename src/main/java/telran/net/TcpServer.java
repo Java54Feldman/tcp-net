73c8a92a73519b78fd0932e72f113b8ba62d0009
Pavel Feldman <feldmanpk@gmail.com>
2024-07-31 17:48:06 +0300

    revert changes

--- src/main/java/telran/net/TcpServer.java
+++ src/main/java/telran/net/TcpServer.java
@@ -1,6 +1,5 @@
 package telran.net;
 
-import java.io.IOException;
 import java.net.*;
 import java.util.*;
 
@@ -8,21 +7,19 @@ public class TcpServer implements Runnable {
 	Protocol protocol;
 	int port;
 	boolean running = true;
-	private static final int ACCEPT_TIMEOUT = 1000;
-	private List<TcpClientServerSession> activeSessions = new ArrayList<>();
-	private ServerSocket serverSocket;
-
+	private static final int ACCEPT_TIMEOUT = 1000; 
+    private List<TcpClientServerSession> activeSessions = new ArrayList<>();
+    
 	public TcpServer(Protocol protocol, int port) {
 		this.protocol = protocol;
 		this.port = port;
 	}
-
-	public void run() {
-		try (ServerSocket serverSocket = new ServerSocket(port)) {
-			// using ServerSocket has the method setSoTimeout
+	public void run() { 
+		try(ServerSocket serverSocket = new ServerSocket(port)) {
+			//using ServerSocket has the method setSoTimeout
 			serverSocket.setSoTimeout(ACCEPT_TIMEOUT);
 			System.out.println("Server is listening on port " + port);
-			while (running) {
+			while(running) {
 				try {
 					Socket socket = serverSocket.accept();
 					TcpClientServerSession session = new TcpClientServerSession(socket, protocol);
@@ -32,27 +29,19 @@ public class TcpServer implements Runnable {
 
 				}
 			}
-
-		} catch (Exception e) {
+			
+		} catch(Exception e) {
 			throw new RuntimeException(e);
 		} finally {
 			shutdown();
-		}
+        }
 	}
-
 	public void shutdown() {
 		running = false;
-		try {
-			if (serverSocket != null && !serverSocket.isClosed()) {
-				serverSocket.close();
-			}
-		} catch (IOException e) {
-			System.out.println("Error closing ServerSocket: " + e.getMessage());
-		}
-		for (TcpClientServerSession session : activeSessions) {
-			session.shutdown();
-		}
-		activeSessions.clear();
+        for (TcpClientServerSession session : activeSessions) {
+            session.shutdown();
+        }
+        activeSessions.clear();
 	}
-
+	
 }
