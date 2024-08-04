c717eae0853ea18bc45676a77403dac49a1c735e
Pavel Feldman <feldmanpk@gmail.com>
2024-08-04 15:49:52 +0300

    HW #42 ExecutorService

--- src/main/java/telran/net/TcpServer.java
+++ src/main/java/telran/net/TcpServer.java
@@ -1,16 +1,29 @@
 package telran.net;
 import java.net.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import static telran.net.TcpConfigurationProperties.*;
 public class TcpServer implements Runnable{
 	Protocol protocol;
 	int port;
 	boolean running = true;
+	ExecutorService sessionExecutor;
 	public TcpServer(Protocol protocol, int port) {
 		this.protocol = protocol;
 		this.port = port;
+		this.sessionExecutor = Executors.newFixedThreadPool(getNumberOfThreads());
 	}
+
 	public void shutdown() {
 		running = false;
+		sessionExecutor.shutdown();
+		try {
+			sessionExecutor.awaitTermination(SESSION_IDLE_TIMEOUT, TimeUnit.MILLISECONDS);
+		} catch (InterruptedException e) {
+			// no interrupts
+		}
 	}
 	public void run() {
 		try(ServerSocket serverSocket = new ServerSocket(port)){
@@ -24,6 +37,8 @@ public class TcpServer implements Runnable{
 					TcpClientServerSession session =
 							new TcpClientServerSession(socket, protocol, this);
 					session.start();
+					sessionExecutor.execute(session);
+					
 				} catch (SocketTimeoutException e) {
 					
 				}
@@ -35,6 +50,10 @@ public class TcpServer implements Runnable{
 		} catch(Exception e) {
 			throw new RuntimeException(e);
 		}
+	}
+	private int getNumberOfThreads() {
+		Runtime runtime = Runtime.getRuntime();
+		return runtime.availableProcessors();
 	}
 	
 }
