cab780d9b4973ccea6d5c145a8772b521d626df3
Pavel Feldman <feldmanpk@gmail.com>
2024-08-24 15:09:35 +0300

    Updating TcpClient method sendAndReceive for proper working with TCP

--- src/main/java/telran/net/TcpConfigurationProperties.java
+++ src/main/java/telran/net/TcpConfigurationProperties.java
@@ -5,7 +5,7 @@ public interface TcpConfigurationProperties {
 	String REQUEST_DATA_FIELD = "requestData";
 	String RESPONSE_CODE_FIELD = "responseCode";
 	String RESPONSE_DATA_FIELD = "responseData";
-	int SESSION_IDLE_TIMEOUT = 15000;
+	int SESSION_IDLE_TIMEOUT = 60000;
 	int SOCKET_TIMEOUT = 1000;
 	String SHUTDOWN = "shutdown";
 	int MAX_WAITING_TIME_IN_SECONDS = 10;
